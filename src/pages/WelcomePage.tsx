b47240600426c63842111a8f2546ca8179eb6acd
gdgalvis <73660713+gdgalvis@users.noreply.github.com>
2024-11-20 15:22:18 -0500

    Welcome update

--- src/pages/WelcomePage.tsx
+++ src/pages/WelcomePage.tsx
@@ -1,24 +1,25 @@
 
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import ImageContainer from '../components/ImageContainer';
+
 import welcomeImage from '../assets/welcome.png'; 
 const WelcomePage: React.FC = () => {
-  const navigate = useNavigate();
-
-  const handleStart = () => {
-    navigate('/form'); 
+    const navigate = useNavigate();
+  
+    const handleStart = () => {
+      navigate('/form'); 
+    };
+  
+    return (
+      <div className="welcome-container">
+        <img
+          src={welcomeImage}
+          alt="Start Your Journey"
+          className="welcome-image"
+          onClick={handleStart}
+        />
+      </div>
+    );
   };
-
-  return (
-    <div className="welcome-container">
-      <h1 className="welcome-title">Welcome to Storyteca</h1>
-      <ImageContainer currentImage={welcomeImage} />
-      <button className="start-button" onClick={handleStart}>
-        Start Your Journey
-      </button>
-    </div>
-  );
-};
-
-export default WelcomePage;
+  
+  export default WelcomePage;
